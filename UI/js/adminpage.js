74acfc9c975d537c72ae7c651655009623385120
Oluwatosin AKinyele <akinyeleolat2005@gmail.com>
2019-01-24 15:59:06 +0100

    [Feature #163387695] create new party

--- UI/js/adminpage.js
+++ UI/js/adminpage.js
@@ -1,30 +1,30 @@
 // credit w3schools tutorial as a guide
-// modal window
 // Get the modal
-const modal = document.getElementById('myModal');
+var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = () => {
+btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = () => {
+span.onclick = function() {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = event => {
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
 // Toggle Tab
 
 function openTab(evt, tabName) {
