9aae1129486ebc1242788c0aaaa4056ed2cf1b8f
Oluwatosin AKinyele <akinyeleolat2005@gmail.com>
2019-01-24 18:50:22 +0100

    [Feature #163387756] create update party

--- UI/js/adminpage.js
+++ UI/js/adminpage.js
@@ -1,9 +1,16 @@
 // credit w3schools tutorial as a guide
 // Get the modal
+var headerTitle = document.getElementById("myHeader");
 var modal = document.getElementById('myModal');
+
+// Get the form element
+var pName = document.getElementById("pName");
+var hqAddress = document.getElementById("hqAddress");
+var formBtn = document.getElementById("formBtn");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var editBtn = document.getElementById("editParty");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -11,8 +18,17 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
+  headerTitle.innerHTML = "CREATE NEW PARTY";
+  formBtn.value="Create"
 }
 
+editBtn.onclick = function(){
+  modal.style.display = "block";
+  headerTitle.innerHTML = "EDIT PARTY";
+  pName.value = "NYP";
+  hqAddress.innerHTML = "IKEJA,LAGOS";
+  formBtn.value="Update"
+}
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
