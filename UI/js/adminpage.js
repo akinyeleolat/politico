5e532c823220bad0556593e7411e5453c8351262
Oluwatosin AKinyele <akinyeleolat2005@gmail.com>
2019-01-25 07:52:38 +0100

    [Feature #163387973] create view-add  office list

--- UI/js/adminpage.js
+++ UI/js/adminpage.js
@@ -4,13 +4,15 @@ var headerTitle = document.getElementById("myHeader");
 var modal = document.getElementById('myModal');
 
 // Get the form element
-var pName = document.getElementById("pName");
-var hqAddress = document.getElementById("hqAddress");
-var formBtn = document.getElementById("formBtn");
+var pName;
+var hqAddress;
+var officeForm = document.getElementById("getForm");
+
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var editBtn = document.getElementById("editParty");
+var officeBtn = document.getElementById("officeBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -19,19 +21,38 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
   headerTitle.innerHTML = "CREATE NEW PARTY";
-  formBtn.value="Create"
+  officeForm.innerHTML =`<p style="text-align: right"><a href="#"><button class="button_3">Upload Logo</button></a></p>
+  <p><input type="text" id="pName" placeholder="Party Name"></p>
+  <p><textarea  id="hqAddress" placeholder="Headquarter Address"></textarea></p>
+  <p><input type="submit" id="formBtn" value="Create Party"  class="button_1"></p>`
 }
 
 editBtn.onclick = function(){
   modal.style.display = "block";
   headerTitle.innerHTML = "EDIT PARTY";
-  pName.value = "NYP";
-  hqAddress.innerHTML = "IKEJA,LAGOS";
-  formBtn.value="Update"
+  pName = "NYP";
+  hqAddress = "IKEJA,LAGOS";
+  officeForm.innerHTML =`<p style="text-align: right"><a href="#"><button class="button_3">Upload New Logo</button></a></p>
+  <p><input type="text" id="pName" value=${pName} placeholder="Party Name"></p>
+  <p><textarea  id="hqAddress" placeholder="Headquarter Address"> ${hqAddress}</textarea></p>
+  <p><input type="submit" id="formBtn" value="Update Party"  class="button_1"></p>`;
+}
+officeBtn.onclick = function() {
+  modal.style.display = "block";
+  headerTitle.innerHTML  = "CREATE NEW OFFICE";
+  officeForm.innerHTML = `
+  <p><input type="text" id="officeName" placeholder="Office Name"></p>
+  <p><select name="office-type">
+      <option value="Federal">Federal</option>
+      <option value="State">State</option>
+      <option value="LGA">Local Government</option>
+    </select></p>
+  <p><input type="submit" id="office-formBtn" value="Create Office" class="button_1"></p>`
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  officeForm.innerHTML = null;
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -40,7 +61,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
 // Toggle Tab
 
 function openTab(evt, tabName) {
