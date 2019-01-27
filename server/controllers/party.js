9e5da842012be196840630a4a89a8b75153c5765
Oluwatosin AKinyele <akinyeleolat2005@gmail.com>
2019-01-27 01:15:25 +0100

    ft #163497292 create get specific party

--- server/controllers/party.js
+++ server/controllers/party.js
@@ -16,6 +16,36 @@ class PartyController {
       message: 'Retrieved all Parties',
     });
   }
+
+  /**
+ * @function getParty
+ * @memberof PartyController
+ * @static
+ */
+  static getParty (req,res){
+    let { id } = req.params;
+    if(isNaN(id)){
+        res.status(404).send({
+            status: 404,
+            message:'Enter the correct party parameter'
+        })
+        return;
+    } 
+    const partyId = Number(id);
+    const partyDetails = party.find(c => c.id === partyId);
+    if (!partyDetails) {
+      res.status(404).send({
+        status: 404,
+        message: 'The party with given id was not found',
+      });
+      return;
+    }
+    res.status(200).send({
+      status: 200,
+      data: partyDetails,
+      message: `Party's Details Retrieved`,
+    });
+  };
 }
 
 export default PartyController;
