f3f076bd1e12cb0f2e5e1d3cc3a60e68f9a48a4e
Oluwatosin AKinyele <akinyeleolat2005@gmail.com>
2019-01-27 03:02:30 +0100

    ft #163497280 create political party

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import PartyController from '../controllers/party';
+import middlewares from '../middlewares';
 
 const router = express.Router();
 router.get('/parties', PartyController.getAllParty);
-router.get('/parties/:id/',PartyController.getParty);
+router.get('/parties/:id', PartyController.getParty);
+router.post('/parties/', middlewares.validateCreateParty,PartyController.createParty);
 export default router;
