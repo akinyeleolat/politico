469170ab1aea4abcaff4b4acbd034d1afef0f53c
Akinyele Oluwatosin Akintayo <Akinyeleolat2005@gmail.com>
2019-01-31 17:43:37 +0100

    Revert "#163497322 Feature Create Delete Party"

--- server/controllers/party.js
+++ server/controllers/party.js
@@ -1,9 +1,8 @@
-import { party, partyData } from '../models/party';
+import party from '../models/party';
 
 /** party controller class */
 
 class PartyController {
-    
   /**
  * @function getAllParty
  * @memberof PartyController
@@ -66,7 +65,7 @@ class PartyController {
     party.push(newParty);
     return res.status(201).send({
       status: 201,
-      data: newParty,
+      newParty,
       message: 'Party Created',
     });
   };
@@ -76,14 +75,7 @@ class PartyController {
  * @static
  */
 static updateParty(req, res) {
-    let { id } = req.params;
-    if(isNaN(id)){
-        res.status(404).send({
-            status: 404,
-            message:'Enter the correct party parameter'
-        })
-        return;
-    }
+    const { id } = req.params;
     const partyId = Number(id);
     let { partyName, partyDetail } = req.body;
     partyName = partyName ? partyName.toString().replace(/\s+/g, '') : partyName;
@@ -111,38 +103,7 @@ static updateParty(req, res) {
       });
       return;
     }
-  /**
- * @function deleteParty
- * @memberof PartyController
- * @static
- */
-static deleteParty(req,res){
-    let { id } = req.params;
-    if(isNaN(id)){
-        res.status(404).send({
-            status: 404,
-            message:'Enter the correct party parameter'
-        })
-        return;
-    } 
-    const partyId = Number(id);
-    const partyDetails = party.find(c => c.id === partyId);
-    if (!partyDetails) {
-      res.status(404).send({
-        status: 404,
-        message: 'The party with given id was not found',
-      });
-      return;
-    }
-    const filteredParty = party.filter(parties => parties !== partyDetails);
-
-    
-    res.status(200).send({
-      status: 200,
-      message: `${partyDetails.partyName} deleted`,
-    });
-    partyData(filteredParty);
-  };
+  
 }
 
 export default PartyController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,9 @@ import PartyController from '../controllers/party';
 import middlewares from '../middlewares';
 
 const router = express.Router();
-router.get('/parties',PartyController.getAllParty);
+router.get('/parties', PartyController.getAllParty);
 router.get('/parties/:id', PartyController.getParty);
 router.post('/parties/', middlewares.validateCreateParty,PartyController.createParty);
 router.patch('/parties/:id/name', middlewares.validateUpdateParty,PartyController.updateParty);
-router.delete('/parties/:id',PartyController.deleteParty);
 
 export default router;
